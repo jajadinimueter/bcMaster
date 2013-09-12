b571076c8911a4c2ed952d1727d1bfe76040a0b3
PinkSheep <oli.fa@bluewin.chgit config --global user.name PinkSheepgit config --global user.email oli.fa@bluewin.ch>
2013-09-12 21:16:31 +0200

    maven initialised

--- bcMaster/src/main/java/ch/zhaw/mdp/App.java
+++ bcMaster/src/main/java/ch/zhaw/mdp/App.java
@@ -6,6 +6,12 @@ package ch.zhaw.mdp;
  */
 public class App 
 {
+    private boolean something;
+
+    public boolean isSomething() {
+        return something;
+    }
+
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
